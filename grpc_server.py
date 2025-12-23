b8bef2acba4407d5bc434915b4774143b912d740
dontkillmyvibee <lysorryoops@gmail.com>
2025-12-23 18:26:18 +0300

    9.1 Введение в grpcio

---
+++ grpc_server.py
@@ -0,0 +1,26 @@
+import grpc
+from concurrent import futures
+import greeting_pb2
+import greeting_pb2_grpc
+
+
+class GreeterServicer(greeting_pb2_grpc.GreeterServicer):
+    def SayHello(self, request, context):
+        name = request.name
+        message = f"Привет, {name}!"
+        return greeting_pb2.HelloReply(message=message)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+
+    greeting_pb2_grpc.add_GreeterServicer_to_server(GreeterServicer(), server)
+
+    server.add_insecure_port('[::]:50051')
+    print("Запуск сервера на порту 50051...")
+    server.start()
+    server.wait_for_termination()
+
+
+if __name__ == '__main__':
+    serve()
