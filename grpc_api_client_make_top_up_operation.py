f9ad70982ffc3c9ea63f500bfb9bbe2a51ff7d70
dontkillmyvibee <lysorryoops@gmail.com>
2026-01-03 21:26:43 +0300

    Практика использования GRPC API-клиентов:

--- grpc_api_client_make_top_up_operation.py
+++ grpc_api_client_make_top_up_operation.py
@@ -1,10 +1,10 @@
 from clients.grpc.gateway.accounts.client import build_accounts_gateway_grpc_client
-from clients.grpc.gateway.cards.client import build_cards_gateway_grpc_client
+from clients.grpc.gateway.operations.client import build_operations_gateway_grpc_client
 from clients.grpc.gateway.users.client import build_users_gateway_grpc_client
 
 users_gateway_client = build_users_gateway_grpc_client()
-cards_gateway_client = build_cards_gateway_grpc_client()
 accounts_gateway_client = build_accounts_gateway_grpc_client()
+operations_gateway_client = build_operations_gateway_grpc_client()
 
 create_user_response = users_gateway_client.create_user()
 print('Create user response:', create_user_response)
@@ -14,8 +14,8 @@ open_debit_card_account_response = accounts_gateway_client.open_debit_card_accou
 )
 print('Open debit card account response:', open_debit_card_account_response)
 
-issue_physical_card_response = cards_gateway_client.issue_physical_card(
-    user_id=create_user_response.user.id,
+make_top_up_operation_response = operations_gateway_client.make_top_up_operation(
+    card_id=open_debit_card_account_response.account.cards[0].id,
     account_id=open_debit_card_account_response.account.id
 )
-print('Issue physical card response:', issue_physical_card_response)
+print('Make top up operation response:', make_top_up_operation_response)
