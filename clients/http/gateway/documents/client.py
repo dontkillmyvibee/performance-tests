3ccfbb6ebcaa5775868017780c73af30edd3f83a
dontkillmyvibee <lysorryoops@gmail.com>
2026-01-06 16:22:24 +0300

    Практика: Создание билдеров для HTTP API клиентов Locust

--- clients/http/gateway/documents/client.py
+++ clients/http/gateway/documents/client.py
@@ -1,7 +1,8 @@
 from httpx import Response
+from locust.env import Environment
 
 from clients.http.client import HTTPClient, HTTPClientExtensions
-from clients.http.gateway.client import build_gateway_http_client
+from clients.http.gateway.client import build_gateway_http_client, build_gateway_locust_http_client
 from clients.http.gateway.documents.schema import (
     GetTariffDocumentResponseSchema,
     GetContractDocumentResponseSchema
@@ -69,3 +70,13 @@ def build_documents_gateway_http_client() -> DocumentsGatewayHTTPClient:
     :return: Готовый к использованию DocumentsGatewayHTTPClient.
     """
     return DocumentsGatewayHTTPClient(client=build_gateway_http_client())
+
+
+def build_documents_gateway_locust_http_client(environment: Environment) -> DocumentsGatewayHTTPClient:
+    """
+    Функция создаёт экземпляр DocumentsGatewayHTTPClient для использования в Locust.
+
+    :param environment: Окружение Locust для сбора метрик.
+    :return: Готовый к использованию DocumentsGatewayHTTPClient.
+    """
+    return DocumentsGatewayHTTPClient(client=build_gateway_locust_http_client(environment))
--- clients/http/gateway/operations/client.py
+++ clients/http/gateway/operations/client.py
@@ -1,7 +1,8 @@
 from httpx import QueryParams, Response
+from locust.env import Environment
 
 from clients.http.client import HTTPClient, HTTPClientExtensions
-from clients.http.gateway.client import build_gateway_http_client
+from clients.http.gateway.client import build_gateway_http_client, build_gateway_locust_http_client
 from clients.http.gateway.operations.schema import (
     GetOperationReceiptResponseSchema,
     GetOperationResponseSchema,
@@ -348,3 +349,13 @@ def build_operations_gateway_http_client() -> OperationsGatewayHTTPClient:
     :return: Готовый к использованию OperationsGatewayHTTPClient.
     """
     return OperationsGatewayHTTPClient(client=build_gateway_http_client())
+
+
+def build_operations_gateway_locust_http_client(environment: Environment) -> OperationsGatewayHTTPClient:
+    """
+    Функция создаёт экземпляр OperationsGatewayHTTPClient для использования в Locust.
+
+    :param environment: Окружение Locust для сбора метрик.
+    :return: Готовый к использованию OperationsGatewayHTTPClient.
+    """
+    return OperationsGatewayHTTPClient(client=build_gateway_locust_http_client(environment))
